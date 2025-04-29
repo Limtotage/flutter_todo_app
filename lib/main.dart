c4df96a7c901c3ad5ff0d1cb0c841aa665fecb96
LimToTage <abdullahltgyalim@gmail.com>
2025-04-29 08:58:51 +0300

    v0.4 Header Design Finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_todo_app/constants/color.dart';
 import 'package:flutter_todo_app/pages/login_register_page.dart';
+import 'package:hexcolor/hexcolor.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -14,16 +16,52 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    double deviceHeight = MediaQuery.of(context).size.height;
+    double deviceWidth = MediaQuery.of(context).size.width;
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          backgroundColor: HexColor(backGroundColor),
+          body: Container(
+            decoration: const BoxDecoration(
+              color: Colors.green,
+              image: DecorationImage(
+                  image: AssetImage("lib/assets/images/greenHeader.png"),
+                  fit: BoxFit.cover),
+            ),
+            width: deviceWidth,
+            height: deviceHeight / 3,
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(top: 25),
+                  child: Text(
+                    "Bugunun Tarihi",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 40),
+                  child: Text(
+                    "My Todo List",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 36,
+                        fontWeight: FontWeight.bold),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
       ),
-      home: const LoginRagistePage(),
+      //home: const LoginRagistePage(),
     );
   }
 }
